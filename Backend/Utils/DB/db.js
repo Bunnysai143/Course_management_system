6904a84615189acf8cfd2f9084dfd057bf5a751d
saikiranmain1708@gmail.com <saikiranmain1708@gmail.com>
2025-08-15 23:31:31 +0530

    changes

--- Backend/Utils/DB/db.js
+++ Backend/Utils/DB/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// 'mongodb+srv://Bharani_Rayudu:Bharani@cluster0.bsh0jfz.mongodb.net/CMS?retryWrites=true&w=majority&appName=Cluster0'
-// mongodb://localhost:27017/CMS
+
 const db = async () => {    
     try {
         await mongoose.connect(process.env.MONGO_URI);
